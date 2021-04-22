deb43b488f4c0ee5d22e6bed0f8670d2186f5c73
Alay Shah <47639000+alayshahh@users.noreply.github.com>
2021-04-22 14:17:58 -0400

    :chart_with_upwards_trend: Patch values added
    Patch values added and smaller image used 128x128

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,8 @@ import numpy as np
 
 PATH_TO_IMG = "./assets/bird.png"
 PATH_TO_GREYSCALE = "./assets/greyscale_bird.png"
-WIDTH = 256  # x/j
-HEIGHT = 256  # y/i
+WIDTH = 128  # x/j
+HEIGHT = 128  # y/i
 RGB_VALUES = np.load("./assets/rgb_values.npy")
 GREY_VALUES = np.load("./assets/greyscale_values.npy")
 FIVE_MEANS_VALUES = np.load('./assets/five_colored.npy')
